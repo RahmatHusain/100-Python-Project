0f443eef1cfe611532e5f47fbaaaee5ed1009b62
Rahmat Husain <raajhusain5@gmail.com>
2026-06-22 10:13:45 -0700

    Add expense storage using list and dictionary

--- expence tracker/main.py
+++ expence tracker/main.py
@@ -4,3 +4,17 @@ amount = input("Enter expense amount: ")
 category = input("Enter category (food, travel, etc): ")
 
 print(amount, category)
+
+expenses = []
+
+amount = input("Enter amount: ")
+category = input("Enter category: ")
+
+expense = {
+    "amount": amount,
+    "category": category
+}
+
+expenses.append(expense)
+
+print(expenses)
