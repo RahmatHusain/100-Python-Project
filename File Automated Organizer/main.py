d519561f061f91f3f9278ff9c3d2313b11bcff89
Rahmat Husain <raajhusain5@gmail.com>
2026-06-23 08:24:41 -0700

    Move files into category folders

--- File Automated Organizer/main.py
+++ File Automated Organizer/main.py
@@ -1,11 +1,17 @@
 import os
 import shutil
-
 path = input("Enter your path : ")
+
 
 files = os.listdir(path)
 for i in files:
     print(i)
+
 for i in files:
     filename , extension = os.path.splitext(i)
     extension_1 = extension[1:]
+    folder_path = path+"\\"+extension_1
+    if os.path.exists(folder_path):
+        shutil.move(path+"\\"+i, path+"\\"+extension_1+"\\"+i)
+    else:
+        os.makedirs(folder_path)
