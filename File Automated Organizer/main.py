24051854d4ef4f10f4c4984780d156702bd58cbf
Rahmat Husain <raajhusain5@gmail.com>
2026-06-23 08:22:45 -0700

    Create category folders automatically

--- File Automated Organizer/main.py
+++ File Automated Organizer/main.py
@@ -6,3 +6,6 @@ path = input("Enter your path : ")
 files = os.listdir(path)
 for i in files:
     print(i)
+for i in files:
+    filename , extension = os.path.splitext(i)
+    extension_1 = extension[1:]
