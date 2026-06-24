c516a0384ef1bd534217b2adbc855c5aa64a526a
Rahmat Husain <raajhusain5@gmail.com>
2026-06-24 05:50:22 -0700

    Add MD5 hashing function

--- duplicate file finder/main.py
+++ duplicate file finder/main.py
@@ -5,3 +5,20 @@ folder_path = "test_files"
 files = os.listdir(folder_path)
 
 print(files)
+
+def get_file_hash(file_path):
+
+    hasher = hashlib.md5()
+
+    with open(file_path, "rb") as file:
+
+        while True:
+
+            chunk = file.read(4096)
+
+            if not chunk:
+                break
+
+            hasher.update(chunk)
+
+    return hasher.hexdigest()
