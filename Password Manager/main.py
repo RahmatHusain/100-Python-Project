06a537e46880c06f8a5d438417de0109300dd09f
Rahmat Husain <raajhusain5@gmail.com>
2026-06-25 04:08:45 -0700

    Add main menu interface

---
+++ Password Manager/main.py
@@ -0,0 +1,12 @@
+print("===== PASSWORD MANAGER =====")
+while True:
+    print("\n===== PASSWORD MANAGER =====")
+    print("1. Add Password")
+    print("2. View Password")
+    print("3. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "3":
+        print("Goodbye!")
+        break
